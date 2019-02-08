a7b80caed049ec382448b65d6cd9a8ca49e8d544
evafinn <15172899@studentmail.ul.ie>
2019-02-08 15:03:40 +0000

    Added dashboard view model

---
+++ ImDiabetic/ViewModels/DashboardViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using ImDiabetic.Models;
+using Realms;
+
+namespace ImDiabetic.ViewModels
+{
+    public class DashboardViewModel
+    {
+        Realm realm;
+        public User User { get; set; }
+        public string Welcome { get; set; }
+        public string Test { get; set; }
+        public Log FirstLog { get; set; }
+
+        public DashboardViewModel(User user)
+        {
+            var config = new RealmConfiguration() { SchemaVersion = 2 };
+            realm = Realm.GetInstance(config);
+            User = user;
+            Welcome = "Welcome " + User.FirstName + "!";
+            HasLogs();
+        }
+
+        public void HasLogs() {
+            var logs = realm.All<Log>().Where(l => l.UserId == User.Id);
+            if (logs.Count() < 1)
+            {
+                Test = "No logs made today";
+            }
+            else
+            {
+                foreach (Log log in logs)
+                {
+                    Debug.WriteLine("*********** LOGS " + log.Id + ", " + log.BloodGlucose + ", " + log.LogDate);
+                }
+                FirstLog = logs.FirstOrDefault();
+                Test = FirstLog.Id + "," + FirstLog.BloodGlucose;
+            }
+        }
+    }
+}
--- ImDiabetic/Views/DashboardPage.xaml.cs
+++ ImDiabetic/Views/DashboardPage.xaml.cs
@@ -3,58 +3,36 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using ImDiabetic.Models;
-using Realms;
+using ImDiabetic.ViewModels;
 using Xamarin.Forms;
 
 namespace ImDiabetic.Views
 {
     public partial class DashboardPage : ContentPage
     {
-        Realm realm;
         public User User { get; }
 
         public DashboardPage(User user)
         {
+            //haven't finished extracting to viewmodel, binding in xaml file for UI objects?!?!?!?!
             NavigationPage.SetHasNavigationBar(this, false);
             InitializeComponent();
-            var config = new RealmConfiguration() { SchemaVersion = 2 };
-            realm = Realm.GetInstance(config);
-            User = user;
-            welcomeUserLabel.Text = "Welcome " + User.FirstName + "!";
+            BindingContext = new DashboardViewModel(user);
+            User = (BindingContext as DashboardViewModel).User;
 
             TapOptions();
-            CheckLogs();
-        }
-
-        private void CheckLogs()
-        {
-            var logs = realm.All<Log>().Where(l => l.UserId == User.Id);
-            if (logs.Count() < 1)
-            {
-                logTestLabel.Text = "No logs made.";
-            } else {
-                foreach(Log log in logs) {
-                    Debug.WriteLine("*********** LOGS " + log.Id + ", " + log.BloodGlucose + ", " + log.LogDate);
-                }
-                Log testLog = logs.FirstOrDefault();
-                logTestLabel.Text = testLog.Id + "," + testLog.BloodGlucose;
-            }
-
         }
 
         private void TapOptions()
         {
-
             imgGoals.Source = ImageSource.FromResource("ImDiabetic.Icons.trophiesStep.png");
             imgLogs.Source = ImageSource.FromResource("ImDiabetic.Icons.logs.png");
             imgTrends.Source = ImageSource.FromResource("ImDiabetic.Icons.trends.png");
             imgMore.Source = ImageSource.FromResource("ImDiabetic.Icons.more.png");
-            //imgHome.Source = ImageSource.FromResource("ImDiabetic.Icons.home.png");
 
             var goalsTap = new TapGestureRecognizer();
             goalsTap.Tapped += async (sender, e) =>
             {
-                //DefaultBackground();
                 stckGoals.BackgroundColor = Color.DeepSkyBlue;
                 await Navigation.PushAsync(new GoalsPage());
                 DefaultBackground();
@@ -64,7 +42,6 @@ namespace ImDiabetic.Views
             var logsTap = new TapGestureRecognizer();
             logsTap.Tapped += async (sender, e) =>
             {
-                //DefaultBackground();
                 stckLogs.BackgroundColor = Color.DeepSkyBlue;
                 await Navigation.PushAsync(new LogsPage(User));
                 DefaultBackground();
@@ -74,7 +51,7 @@ namespace ImDiabetic.Views
             var trendsTap = new TapGestureRecognizer();
             trendsTap.Tapped += async (sender, e) =>
             {
-                //DefaultBackground();
+                ;
                 stckTrends.BackgroundColor = Color.DeepSkyBlue;
                 await Navigation.PushAsync(new TrendPage());
                 DefaultBackground();
@@ -84,22 +61,11 @@ namespace ImDiabetic.Views
             var moreTap = new TapGestureRecognizer();
             moreTap.Tapped += async (sender, e) =>
             {
-                //DefaultBackground();
                 stckMore.BackgroundColor = Color.DeepSkyBlue;
                 await Navigation.PushAsync(new MorePage());
                 DefaultBackground();
             };
             stckMore.GestureRecognizers.Add(moreTap);
-
-            //var homeTap = new TapGestureRecognizer();
-            //homeTap.Tapped += async (sender, e) =>
-            //{
-            //    //DefaultBackground();
-            //    stckHome.BackgroundColor = Color.DeepSkyBlue;
-            //    await Navigation.PushAsync(new DashboardPage(User));
-            //    DefaultBackground();
-            //};
-            //stckHome.GestureRecognizers.Add(homeTap);
         }
 
         public void DefaultBackground()
@@ -109,12 +75,6 @@ namespace ImDiabetic.Views
             stckTrends.BackgroundColor = Color.White;
             stckMore.BackgroundColor = Color.White;
         }
-
-        //void Handle_ItemSelected(object sender, Xamarin.Forms.SelectedItemChangedEventArgs e)
-        //{
-        //    Detail =new NavigationPage(new ProfilePage(User));
-        //    IsPresented = false;
-        //}
 
         protected override bool OnBackButtonPressed()
         {
